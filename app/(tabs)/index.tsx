620a0449a10f75c515e86dede0e8e942047ecb5c
Adi Fatkhurozi <ady.fatkhurozi@gmail.com>
2024-06-12 13:10:41 +0700

    commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,106 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, StyleSheet, Text, View, TextInput } from 'react-native';
 
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import { FormGenerator, FormProvider, FormUiProvider, SchemaVariant, useField, useForm } from 'form-trial';
+
+export const FuiFieldText: React.FC<any> = ({ schema, wrapper: Wrapper, schemas }) => {
+  const { ref, formState, state, onChange, getProp } = useField({ schema });
+  const hidden = getProp('hidden');
+  if (hidden) return <></>;
+
+  const style = getProp('style');
+  const placeholder = getProp('placeholder');
+  const title = getProp('title');
+  const subtitle = getProp('subtitle');
+  const optional = getProp('optional');
+  const format = getProp('format');
+  const mask = getProp('mask');
+  const hint = getProp('hint');
+  const tooltip = getProp('tooltip');
+  const disabled = getProp('disabled');
+  const suffix = getProp('suffix');
+  const prefix = getProp('prefix');
+  const touched = getProp('touched');
+  const { isSubmitted, isForceSubmitted } = formState;
+  const hasTouched = (isSubmitted && !isForceSubmitted) || touched || state.touched;
+  const error = hasTouched && state.error;
+
+  console.log('value changed', state.value)
+  return (
+    <Wrapper schema={schema} schemas={schemas} style={style}>
+      <TextInput
+        placeholder={placeholder}
+        value={state.value}
+        onChangeText={(value) => {
+          console.log('onchange coming', value)
+          onChange(value)
+        }}
+      />
+    </Wrapper>
+  )
+}
+
+
+const components: any = {
+  [SchemaVariant.FIELD]: {
+    ['TEXT']: FuiFieldText
+  },
+  [SchemaVariant.VIEW]: {},
+  [SchemaVariant.FIELD_ARRAY]: {},
+  [SchemaVariant.FIELD_OBJECT]: {},
+  [SchemaVariant.GROUP]: {},
+};
+const schemas: any = [
+  {
+    "variant": "FIELD",
+    "component": "TEXT",
+    "config": {
+      "name": "text",
+      "defaultValue": ""
+    },
+    "properties": {
+      "placeholder": {
+        "value": "Placeholder",
+      },
+      "title": {
+        "value": "Title"
+      },
+      "style": {
+        "value": {
+          "lg": 12,
+          "md": 12,
+          "sm": 12
+        }
+      }
+    },
+    "rules": [],
+    "id": "mXxQnyBkd7"
+  }
+];
+const initialValues = {};
+const extraData = {}
+
+export const BasicExample = () => {
+  const actions = useForm({
+    schemas,
+    initialValues,
+    extraData
+  })
+
+  return (
+    <FormProvider value={actions}>
+      <FormUiProvider value={{ components }}>
+        <FormGenerator
+          wrapper={({ children }: any) => <View>{children}</View>}
+          error={() => <Text>Sample Error</Text>}
+        />
+      </FormUiProvider>
+    </FormProvider>
+  )
+}
 
 export default function HomeScreen() {
   return (
@@ -16,36 +113,13 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title">Welcome xxxx!</ThemedText>
         <HelloWave />
       </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
+      <View>
+        <BasicExample />
+      </View>
+      <Text>Bottom</Text>
     </ParallaxScrollView>
   );
 }
